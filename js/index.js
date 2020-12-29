cd9577ab1c3ecad36846eada09651893d9337d7d
Иван <kleimenov416@gmail.com>
2020-12-29 09:49:11 -0500

    start working at device oStore page, implement js in list items

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 
+let checkedBullet = document.querySelectorAll('.slider-nav-bullet');
+
+for (let bullet in checkedBullet) {
+    checkedBullet[bullet].addEventListener('click', (evt) => {
+        evt.preventDefault();
+        checkedBullet[bullet].classList.toggle('slider-nav-bullet-checked')
+    })
+}
