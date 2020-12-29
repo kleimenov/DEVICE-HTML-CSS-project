7a586601dd413767e24401da68819bf6f5dc1b8b
Иван <kleimenov416@gmail.com>
2020-12-29 10:07:49 -0500

    start working at device oStore page, implement js in list items

--- js/index.js
+++ js/index.js
@@ -1,9 +1,25 @@
 
-let checkedBullet = document.querySelectorAll('.slider-nav-bullet');
+let bullets = document.querySelectorAll('.slider-nav-bullet');
 
-for (let bullet in checkedBullet) {
-    checkedBullet[bullet].addEventListener('click', (evt) => {
+
+
+
+
+for (let index in checkedBullet) {
+    index.addEventListener('click', (evt) => {
         evt.preventDefault();
-        checkedBullet[bullet].classList.toggle('slider-nav-bullet-checked')
+        if(!index.classList.contains('slider-nav-bullet-checked')) {
+            index.classList.add('slider-nav-bullet-checked')
+        } else {
+            index.classList.remove('slider-nav-bullet-checked')
+        }
     })
 }
+
+/*
+checkedBullet[bullet].addEventListener('click', (evt) => {
+    evt.preventDefault();
+    checkedBullet[bullet].classList.toggle('slider-nav-bullet-checked')
+    console.log('hello')
+})
+*/
